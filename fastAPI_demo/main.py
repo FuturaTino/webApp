a9b442832b14c3b93ebcd85bf9d631fdd3e05516
FuturaTino <63669705@qq.com>
2024-01-05 21:32:47 +0800

    Remove unnecessary files and code

--- fastAPI_demo/main.py
+++
@@ -1,8 +0,0 @@
-from typing import Union 
-from fastapi import FastAPI ,Depends, HTTPException, status 
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel 
-
-app = FastAPI()
-
-
