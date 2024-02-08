182eaf7d61cc6d5d0ba6a3500b037bc8652dfc67
FuturaTino <63669705@qq.com>
2024-02-08 22:08:41 +0800

    前端项目

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div >
+      <HelloWorld/>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
