c000710a472e10186ccd5d8f00286e9700902fef
FuturaTino <63669705@qq.com>
2024-03-01 15:04:48 +0800

    任务节点，更新任务，将预训练和训练合二为一

--- node/celeryApp.py
+++ node/celeryApp.py
@@ -27,24 +27,13 @@ app = Celery(__name__,
 
 # celery settings from https://docs.celeryq.dev/en/stable/userguide/configuration.html
 app.conf.broker_connection_retry_on_startup = True
-app.conf.worker_max_tasks_per_child = 2 # PoolWorker子进程在执行n个任务后重启，可以防止内存泄露。
+app.conf.worker_max_tasks_per_child = 1 # PoolWorker子进程在执行1个任务后重启，可以防止内存泄露。
 app.conf.worker_concurrency = 2
-# About routes https://docs.celeryq.dev/en/stable/userguide/routing.html
-# app.conf.task_default_exchange = 'default'
-# app.conf.task_default_queue = 'default'
 app.conf.task_default_exchange_type = 'fanout' # 广播模式
-app.conf.task_queues = (
-    Queue('colmap', routing_key='colmap.low'), # exchange:实体交换机 ；交换机中的routing_key 永远区分 一个celery中的多个队列（优先级）
-    Queue('gs', routing_key='gs.low')
-)
 
-class colmapTask(Task):
-    def __init__(self) -> None:
-        root_dir = Path(__file__).parent # app dir
-        self.sotrage_dir = root_dir / Path(os.environ.get('STORAGE_DIR'))   
 
+class ReconstructTask(Task):
     def before_start(self, task_id, args, kwargs):
-
         update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['PreProcessing'])
         try:
             prepare_job(uuid=task_id)
@@ -55,77 +44,40 @@ class colmapTask(Task):
             raise e
 
     def on_success(self, retval, task_id, args, kwargs):
-        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Queue_2'])
-        print(f"Task: {task_id},Status: {STATUS['Queue_2']} ✅💨 ")
+        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Success'])
+        print(f"Task: {task_id},Status: {STATUS['Success']} ✅ ")
+        work_dir = Path(os.getenv('STORAGE_DIR')) / task_id
+        delete_local_dir(work_dir)
+
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
         update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Failed'])
-        print(f"Task: {task_id},Status: {STATUS['Failed']}")
-    
-class gsTask(Task):
-    def __init__(self) -> None:
-        root_dir = Path(__file__).parent # app dir
-        self.sotrage_dir = root_dir / Path(os.environ.get('STORAGE_DIR'))  
-
-    def before_start(self, task_id, args, kwargs):
-        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Reconstructing'])
-        print(f"Task: {task_id},Status: {STATUS['Reconstructing']}")
-
-    def on_success(self, retval, task_id, args, kwargs):
-        try:
-            work_dir = Path(os.getenv('STORAGE_DIR')) / task_id
-            delete_local_dir(work_dir)
-            image_url = get_oss_image_url(object=task_id)
-            ply_url = get_oss_ply_url(object_name=task_id)
-            update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Success'])
-            update_capture_info(db=next(get_db()),uuid=task_id,image_url=image_url,ply_url=ply_url)
-            print(f"Task: {task_id},Status: {STATUS['Success']} ✅ ")
-        except Exception as e:
-            update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Failed'])
-            raise e
-        
-    def on_failure(self, exc, task_id, args, kwargs, einfo):
-        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Failed'])
+        work_dir = Path(os.getenv('STORAGE_DIR')) / task_id
+        delete_local_dir(work_dir)
         print(f"Task: {task_id},Status: {STATUS['Failed']}")
 
-@app.task(bind=True, time_limit=60*60,base=colmapTask) # bind=True 会将task(这里是customTask)实例作为第一个参数传入
-def process(self,uuid):
-    # 获取视频文件,放到storage_path
-    storage_path = self.sotrage_dir / uuid
-    video_path:Path = storage_path / f"{uuid}.mp4"
+@app.task(bind=True, time_limit=60*60,base=ReconstructTask) # bind=True 会将task(这里是customTask)实例作为第一个参数传入
+def Reconstruct(self,uuid):
+    work_dir:Path = Path(os.getenv('STORAGE_DIR')) / uuid
+    video_path:Path = work_dir / f"{uuid}.mp4"
+    ply_path = work_dir / f"{uuid}.ply"
+    image_dir = work_dir / "input"
     if not video_path.exists():
-        raise Exception(f"{video_path} does't exist")
-    image_dir  = storage_path /  "input"
-    convert_video_to_images(video_path=video_path, image_dir=image_dir,num_frames_target=320,num_downscales=1)
-    convert_images_to_colmap(source_path=storage_path,verbose=False,no_gpu=False)
-    try:
-        pass
-        # os.remove(video_path)
-    except: 
-        print(f"{video_path} does't exist")
-    # todo: 修正照片质量
-
-@app.task(bind=True, time_limit=60*60,base=gsTask)
-def reconstruct(self,uuid):
-    try:
-        storage_dir = Path(os.getenv('STORAGE_DIR'))
-        work_dir = storage_dir / uuid
-        ply_path = storage_dir / uuid / (uuid+'.ply')
-        training(source_path=work_dir,model_path=work_dir,uuid=uuid,saving_iterations=[30000])
-        # 将重建结果文件上传oss
-        if not ply_path.exists():
-            raise Exception(f"{ply_path} does't exist")
-        oss_ply_key = "ply/" + uuid + ".ply"
-        upload_file(oss_key=oss_ply_key, local_path=str(ply_path))
-    except Exception as e:
-        torch.cuda.empty_cache()
         update_capture_status(db=next(get_db()),uuid=uuid,status=STATUS['Failed'])
+        raise FileNotFoundError(f"Video file not found: {video_path}")
+
+    convert_video_to_images(video_path=video_path,image_dir=image_dir,num_frames_target=320)
+    convert_images_to_colmap(source_path=work_dir,no_gpu=False,berbose=False)
+    
+
+    # train model ,uplaod the ply  and share the url
+    update_capture_status(db=next(get_db()),uuid=uuid,status=STATUS['Reconstructing'])
+    try:
+        training(source_path=work_dir,model_path=work_dir, uuid=uuid,saving_iterations=[30000])
+        upload_file(oss_key=f"ply/{uuid}.ply",local_path=str*(ply_path))
+        image_url = get_oss_image_url(object_name=uuid)
+        result_url = get_oss_ply_url(object_name=uuid)
+        update_capture_info(db=next(get_db()),uuid=uuid,image_url=image_url,result_url=result_url)
+    except Exception as e:
         raise e
-
-    
-
-    
-    
-    
-    
 
---
+++ node/celeryApp2.py
@@ -0,0 +1,131 @@
+from celery import Celery
+from celery import Task 
+import time
+from dotenv import load_dotenv,find_dotenv
+import os
+from pathlib import Path
+import shutil 
+from kombu import Queue
+from core.colmap.convert import convert_video_to_images,convert_images_to_colmap
+from core.dependencies import get_db
+from core.oss import upload_file,prepare_job,delete_local_dir,get_oss_image_url,get_oss_ply_url
+from core.reconstruct.train import training
+
+from crud.captures import update_capture_status,STATUS,update_capture_info
+import torch 
+
+load_dotenv(find_dotenv("config.env"))
+username = os.environ.get("REDIS_USER")
+password = os.environ.get("REDIS_PASSWORD")
+host = os.environ.get("REDIS_HOST")
+port = os.environ.get("REDIS_PORT")
+
+app = Celery(__name__,  
+             broker=f'redis://{username}:{password}@{host}:{port}/0',
+             backend=f'redis://{username}:{password}@{host}:{port}/1')
+
+
+# celery settings from https://docs.celeryq.dev/en/stable/userguide/configuration.html
+app.conf.broker_connection_retry_on_startup = True
+app.conf.worker_max_tasks_per_child = 2 # PoolWorker子进程在执行n个任务后重启，可以防止内存泄露。
+app.conf.worker_concurrency = 2
+# About routes https://docs.celeryq.dev/en/stable/userguide/routing.html
+# app.conf.task_default_exchange = 'default'
+# app.conf.task_default_queue = 'default'
+app.conf.task_default_exchange_type = 'fanout' # 广播模式
+app.conf.task_queues = (
+    Queue('colmap', routing_key='colmap.low'), # exchange:实体交换机 ；交换机中的routing_key 永远区分 一个celery中的多个队列（优先级）
+    Queue('gs', routing_key='gs.low')
+)
+
+class colmapTask(Task):
+    def __init__(self) -> None:
+        root_dir = Path(__file__).parent # app dir
+        self.sotrage_dir = root_dir / Path(os.environ.get('STORAGE_DIR'))   
+
+    def before_start(self, task_id, args, kwargs):
+
+        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['PreProcessing'])
+        try:
+            prepare_job(uuid=task_id)
+            print(f"Task: {task_id},Status: {STATUS['PreProcessing']}")
+        except Exception as e:
+            update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Failed'])
+            print(f"Task: {task_id},Status: {STATUS['Failed']}")
+            raise e
+
+    def on_success(self, retval, task_id, args, kwargs):
+        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Queue_2'])
+        print(f"Task: {task_id},Status: {STATUS['Queue_2']} ✅💨 ")
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Failed'])
+        print(f"Task: {task_id},Status: {STATUS['Failed']}")
+    
+class gsTask(Task):
+    def __init__(self) -> None:
+        root_dir = Path(__file__).parent # app dir
+        self.sotrage_dir = root_dir / Path(os.environ.get('STORAGE_DIR'))  
+
+    def before_start(self, task_id, args, kwargs):
+        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Reconstructing'])
+        print(f"Task: {task_id},Status: {STATUS['Reconstructing']}")
+
+    def on_success(self, retval, task_id, args, kwargs):
+        try:
+            work_dir = Path(os.getenv('STORAGE_DIR')) / task_id
+            delete_local_dir(work_dir)
+            image_url = get_oss_image_url(object=task_id)
+            ply_url = get_oss_ply_url(object_name=task_id)
+            update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Success'])
+            update_capture_info(db=next(get_db()),uuid=task_id,image_url=image_url,ply_url=ply_url)
+            print(f"Task: {task_id},Status: {STATUS['Success']} ✅ ")
+        except Exception as e:
+            update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Failed'])
+            raise e
+        
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        update_capture_status(db=next(get_db()),uuid=task_id,status=STATUS['Failed'])
+        print(f"Task: {task_id},Status: {STATUS['Failed']}")
+
+@app.task(bind=True, time_limit=60*60,base=colmapTask) # bind=True 会将task(这里是customTask)实例作为第一个参数传入
+def process(self,uuid):
+    # 获取视频文件,放到storage_path
+    storage_path = self.sotrage_dir / uuid
+    video_path:Path = storage_path / f"{uuid}.mp4"
+    if not video_path.exists():
+        raise Exception(f"{video_path} does't exist")
+    image_dir  = storage_path /  "input"
+    convert_video_to_images(video_path=video_path, image_dir=image_dir,num_frames_target=320,num_downscales=1)
+    convert_images_to_colmap(source_path=storage_path,verbose=False,no_gpu=False)
+    try:
+        pass
+        # os.remove(video_path)
+    except: 
+        print(f"{video_path} does't exist")
+    # todo: 修正照片质量
+
+@app.task(bind=True, time_limit=60*60,base=gsTask)
+def reconstruct(self,uuid):
+    try:
+        storage_dir = Path(os.getenv('STORAGE_DIR'))
+        work_dir = storage_dir / uuid
+        ply_path = storage_dir / uuid / (uuid+'.ply')
+        training(source_path=work_dir,model_path=work_dir,uuid=uuid,saving_iterations=[30000])
+        # 将重建结果文件上传oss
+        if not ply_path.exists():
+            raise Exception(f"{ply_path} does't exist")
+        oss_ply_key = "ply/" + uuid + ".ply"
+        upload_file(oss_key=oss_ply_key, local_path=str(ply_path))
+    except Exception as e:
+        torch.cuda.empty_cache()
+        update_capture_status(db=next(get_db()),uuid=uuid,status=STATUS['Failed'])
+        raise e
+
+    
+
+    
+    
+    
+    
+
